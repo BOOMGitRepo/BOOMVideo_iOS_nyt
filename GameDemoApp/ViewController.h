79cb2f6e565d1fd1d686cdf5c7c711c9c1574e07
BoomVideo <kate@boomvideo.com.au>
2015-08-12 13:20:37 +1000

    import files

---
+++ GameDemoApp/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  GameDemoApp
+//
+//  Created by Boom on 19/01/15.
+//  Copyright (c) 2015 BM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BMResourceManager.h"
+#import "YTPlayerView.h"
+
+@interface ViewController : UIViewController <BoomVideoTrackerDelegate>
+
+@end
+
